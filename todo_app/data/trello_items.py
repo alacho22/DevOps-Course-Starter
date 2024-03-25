506967aaf55388521d8f6e9b9935964ce2399833
Alan Chodyniecki <alan.chodyniecki@softwire.com>
2024-03-25 16:57:29 +0000

    feat: add implementation of add_item in trello_items

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,3 +1,8 @@
+import os
+import json
+import requests
+
+
 def get_items():
     """
     Fetches all saved items from the session.
@@ -18,4 +23,29 @@ def add_item(title):
     Returns:
         item: The saved item.
     """
-    raise NotImplementedError
+    url = "https://api.trello.com/1/cards"
+
+    headers = {"Accept": "application/json"}
+
+    list_id_to_add_to = os.getenv("TRELLO_LIST_ID")
+    api_key = os.getenv("TRELLO_API_KEY")
+    api_token = os.getenv("TRELLO_API_TOKEN")
+
+    query = {
+        "idList": list_id_to_add_to,
+        "key": api_key,
+        "token": api_token,
+        "name": title,
+    }
+
+    response = requests.request("POST", url, headers=headers, params=query, timeout=3)
+
+    response.raise_for_status()
+
+    item_response = json.loads(response.text)
+
+    return {
+        "id": item_response["id"],
+        "title": item_response["name"],
+        "status": "Not Started",
+    }
