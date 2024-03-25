86906d5f9044afa06578419571807a34d4627b1f
Alan Chodyniecki <alan.chodyniecki@softwire.com>
2024-03-25 17:38:28 +0000

    feat: add initial implementation of trello_items get_items

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import os
 import json
 import requests
@@ -10,7 +11,44 @@ def get_items():
     Returns:
         list: The list of saved items.
     """
-    raise NotImplementedError
+    board_id = os.getenv("TRELLO_BOARD_ID")
+    url = f"https://api.trello.com/1/boards/{board_id}/lists"
+
+    headers = {"Accept": "application/json"}
+
+    api_key = os.getenv("TRELLO_API_KEY")
+    api_token = os.getenv("TRELLO_API_TOKEN")
+    query = {"key": api_key, "token": api_token, "cards": "open"}
+
+    response = requests.request("GET", url, headers=headers, params=query, timeout=3)
+
+    response.raise_for_status()
+
+    board_lists_response = json.loads(response.text)
+    cards_per_status = [
+        board_list_to_cards(board_list_response)
+        for board_list_response in board_lists_response
+    ]
+
+    all_cards = reduce(list.__add__, cards_per_status)
+
+    return all_cards
+
+
+def board_list_to_cards(board_list_response):
+    status = board_list_response["name"]
+    return [
+        card_response_to_card(status, card_response)
+        for card_response in board_list_response["cards"]
+    ]
+
+
+def card_response_to_card(status, card_response):
+    return {
+        "id": card_response["id"],
+        "title": card_response["name"],
+        "status": status,
+    }
 
 
 def add_item(title):
@@ -42,10 +80,10 @@ def add_item(title):
 
     response.raise_for_status()
 
-    item_response = json.loads(response.text)
+    card_response = json.loads(response.text)
 
     return {
-        "id": item_response["id"],
-        "title": item_response["name"],
+        "id": card_response["id"],
+        "title": card_response["name"],
         "status": "Not Started",
     }
