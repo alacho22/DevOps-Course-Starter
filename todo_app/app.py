cb018ceef9ee1466fe324514e3694c032bd7092f
Alan Chodyniecki <alan.chodyniecki@softwire.com>
2024-05-17 14:50:04 +0000

    add create_app()

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,25 +4,26 @@ from todo_app.data.trello_items import add_item, complete_item, get_items
 from todo_app.flask_config import Config
 from todo_app.models.view import ViewModel
 
-app = Flask(__name__)
-app.config.from_object(Config())
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
-@app.route("/")
-def index():
-    items = get_items()
-    item_view_model = ViewModel(items)
-    return render_template("index.html", view_model=item_view_model)
+    @app.route("/")
+    def index():
+        items = get_items()
+        item_view_model = ViewModel(items)
+        return render_template("index.html", view_model=item_view_model)
 
+    @app.route("/item", methods=["POST"])
+    def item():
+        title = request.form.get("title")
+        add_item(title)
+        return redirect("/")
 
-@app.route("/item", methods=["POST"])
-def item():
-    title = request.form.get("title")
-    add_item(title)
-    return redirect("/")
+    @app.route("/item/<id>/complete", methods=["POST"])
+    def complete_item_action(id):
+        complete_item(id)
+        return redirect("/")
 
-
-@app.route("/item/<id>/complete", methods=["POST"])
-def complete_item_action(id):
-    complete_item(id)
-    return redirect("/")
+    return app
